d05333340aa8dadbb40a16d2d2f377cf568d8966
gokeay <ozcang20@itu.edu.tr>
2023-03-24 21:30:20 +0300

    added new files

---
+++ musiChain-blockend/contracts/ForArtist.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// import "hardhat/console.sol";
+
+contract Lock {
+
+    constructor() {
+
+    }
+
+    function creat_acount()
+
+
+
+
+}
