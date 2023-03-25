ca81dec4598282c0f3842f689dae31651ae762ec
Ömer <109610497+jemapelleomer@users.noreply.github.com>
2023-03-25 23:43:25 +0300

    Create constants.js

---
+++ musiChain-frontend/constants.js
@@ -0,0 +1,15 @@
+// MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
+export const INFURA_ID = "460f40a260564ac4a4f4b3fffb032dad";
+
+// MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
+export const ETHERSCAN_KEY = "QHE2IGTVMDPJGZ1Y4JJFIDEPF3UFN15KND";
+
+export const NETWORKS = {
+    scrollAlpha: {
+        name: "scrollAlpha",
+        color: "#e9d0b8",
+        chainId: 534353,
+        rpcUrl: "https://alpha-rpc.scroll.io/l2",
+        blockExplorer: "https://blockscout.scroll.io",
+    },
+};
